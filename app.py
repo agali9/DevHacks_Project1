14ec2dd1f56e4f5b1ff1e75a5369ae1cfec8f0ab
agali9 <agali9@asu.edu>
2025-03-30 11:28:56 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import random
+
+app = Flask(__name__)
+
+player_data = []
+
+@app.route('/main_page')
+def main_page():
+    return render_template('main.html', players=player_data)
+
+@app.route('/race')
+def index():
+    return render_template('Race.html')  
+
+@app.route('/')
+def start():
+    return render_template('start.html')
+
+@app.route('/receive_list', methods=['POST'])
+def receive_list():
+    global player_data
+    player_data = request.json.get('players', [])
+    return jsonify({"message": "Player data received", "data": player_data})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
